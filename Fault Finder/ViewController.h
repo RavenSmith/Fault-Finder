57b337555415fbcd7520286ef75c7db6e936abab
Raven Smith <raven.smith@willowtreeapps.com>
2015-05-30 12:39:52 -0400

    Initial Commit

---
+++ Fault Finder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Fault Finder
+//
+//  Created by Raven Smith on 5/30/15.
+//  Copyright (c) 2015 Raven Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Fault Finder/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Fault Finder
+//
+//  Created by Raven Smith on 5/30/15.
+//  Copyright (c) 2015 Raven Smith. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
