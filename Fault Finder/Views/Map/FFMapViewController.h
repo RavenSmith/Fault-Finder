03a450245ff48148623ccb13f29e3acaca91b1be
Raven Smith <raven.smith@willowtreeapps.com>
2015-06-05 12:05:42 -0400

    working mapview

---
+++ Fault Finder/Views/Map/FFMapViewController.h
@@ -0,0 +1,13 @@
+//
+//  EarthquakeMapViewController.h
+//  Fault Finder
+//
+//  Created by Raven Smith on 6/5/15.
+//  Copyright (c) 2015 Raven Smith. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface EarthquakeMapViewController : ViewController
+
+@end
