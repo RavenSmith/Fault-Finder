ffc7ffcf85d988d9ba58d73cac9ebb9dfdc1983f
Raven Smith <raven.smith@willowtreeapps.com>
2015-06-08 10:47:49 -0400

    loading screen functionality added

---
+++ Fault Finder/ActivityIndicator/FFActivityViewController.h
@@ -0,0 +1,13 @@
+//
+//  FFActivityViewController.h
+//  Fault Finder
+//
+//  Created by Raven Smith on 6/5/15.
+//  Copyright (c) 2015 Raven Smith. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface FFActivityViewController : ViewController
+
+@end
